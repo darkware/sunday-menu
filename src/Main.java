aa6480f2fba51207979d4d1181d471427b7cf5f6
darkware <darkware.spam@gmail.com>
2014-04-04 10:25:11 +0200

    Finished

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         MainWindow window = new MainWindow();
-
+        window.setVisible(true);
 
     }
 
--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,19 +1,18 @@
 import com.lowagie.text.DocumentException;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPReply;
-import org.xhtmlrenderer.pdf.ITextFontResolver;
 import org.xhtmlrenderer.pdf.ITextRenderer;
-import sun.net.ftp.FtpClient;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
-import java.net.SocketAddress;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.util.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -22,6 +21,7 @@ import java.util.List;
 public class MainWindow extends JFrame {
 
     private static final String titlePlaceholder = "TITLE_FIELD";
+    private static final String datePlaceholder = "DATE_FIELD";
     private static final String starterPlaceholder = "STARTER_FIELD";
     private static final String mainOnePlaceholder = "MAINONE_FIELD";
     private static final String mainTwoPlaceholder = "MAINTWO_FIELD";
@@ -46,13 +46,41 @@ public class MainWindow extends JFrame {
     private JTextField mainOneAdditionTextField;
     private JTextField mainTwoAdditionTextField;
     private JTextField dessertAdditionTextField;
+    private JTextField dateTextField;
 
-    private static final String HOST = "http://127.0.0.1";
-    private static final int PORT = 3360;
+    private static final String HOST = "msplins10.bon.at";
+    private static final int PORT = 21;
 
     public MainWindow() throws HeadlessException {
         super("Sonntagskarten Generator");
+       /* try {
+            UIManager.setLookAndFeel(
+                    UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }*/
         setContentPane(rootPanel);
+
+        Calendar now = Calendar.getInstance();
+        int weekday = now.get(Calendar.DAY_OF_WEEK);
+        if (weekday != Calendar.SUNDAY)
+        {
+            // calculate how much to add
+            // the 2 is the difference between Saturday and Monday
+            int days = (Calendar.SATURDAY - weekday + 1) % 7;
+            now.add(Calendar.DAY_OF_YEAR, days);
+        }
+        // now is the date you want
+        Date date = now.getTime();
+        String format = new SimpleDateFormat("dd.M.yyyy").format(date);
+        dateTextField.setText(format);
+
         pack();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
@@ -63,15 +91,15 @@ public class MainWindow extends JFrame {
         // TODO: place custom component creation code here
     }
 
-    private String PASSWORD = "password";
-    private String USERNAME = "username";
+    private String PASSWORD = "frvofuJx";
+    private String USERNAME = "b976345645@der-wirt.at";
     private ActionListener finishedButtonPressedListener = new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
             try {
                 String inputFile = "html-templates/template.html";
                 File file = new File(inputFile);
-                String outputFile = "firstdoc.pdf";
+                File outputfile = null;
                 List<String> content = Files.readAllLines(file.toPath(), StandardCharsets.UTF_8);
 
                 StringBuffer buf = new StringBuffer();
@@ -79,6 +107,8 @@ public class MainWindow extends JFrame {
                     if (s.contains(titlePlaceholder)) {
                         String text = titleTextField.getText();
                         s = s.replace(titlePlaceholder, text);
+                    } else if (s.contains(datePlaceholder)) {
+                        s = s.replace(datePlaceholder, dateTextField.getText());
                     } else if (s.contains(starterPlaceholder)) {
                         s = s.replace(starterPlaceholder, starterTextField.getText());
                     } else if (s.contains(mainOnePlaceholder)) {
@@ -113,7 +143,7 @@ public class MainWindow extends JFrame {
                 int rVal = c.showSaveDialog(MainWindow.this);
                 if (rVal == JFileChooser.APPROVE_OPTION) {
 
-                    File outputfile = c.getSelectedFile();
+                    outputfile = c.getSelectedFile();
                     if (outputfile != null) {
                         try {
                             OutputStream os = new FileOutputStream(outputfile);
@@ -121,22 +151,21 @@ public class MainWindow extends JFrame {
                             os.close();
 
                             //Check if checkbox is selected
-                            if(shareOnlineCheckBox.isSelected()){
+                            if (shareOnlineCheckBox.isSelected() && outputfile != null) {
                                 //Publish online
                                 FTPClient client = new FTPClient();
-                                client.connect(HOST,PORT);
+                                client.connect(HOST, PORT);
                                 int reply = client.getReplyCode();
-                                if (FTPReply.isPositiveCompletion(reply))
-                                {
-                                    if(client.login(USERNAME, PASSWORD)){
-                                        InputStream in = new FileInputStream(outputFile);
-                                        client.storeFile("sonntagskarte.pdf",in);
+                                if (FTPReply.isPositiveCompletion(reply)) {
+                                    if (client.login(USERNAME, PASSWORD)) {
+                                        InputStream in = new FileInputStream(outputfile);
+                                        client.storeFile("/var/www/html/data/sonntagskarte.pdf", in);
                                         in.close();
-                                    }else{
+                                    } else {
                                         client.logout();
                                         client.disconnect();
                                     }
-                                }else{
+                                } else {
                                     client.disconnect();
                                 }
                             }
